937dca5949c2f023306ee681a6b0cd79fa8467f3
gloureiro100m <gloureiro100@gmail.com>
2018-08-03 00:31:30 -0300

    classes de dominio Cliente, Endereco, Tipo Cliente

---
+++ src/main/java/br/com/gabriel/cursomc/domain/Cliente.java
@@ -0,0 +1,89 @@
+package br.com.gabriel.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.OneToMany;
+
+import br.com.gabriel.cursomc.enums.TipoCliente;
+
+public class Cliente implements Serializable {
+
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfCnpj;
+	private Integer tipo;
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name="TELEFONE")
+	private Set<String> telefones = new HashSet<>();
+	
+	public Cliente() {
+		
+	}
+	
+	
+
+	public Cliente(Integer id, String nome, String email, String cpfCnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfCnpj = cpfCnpj;
+		this.tipo = tipo.getCod();
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(this.tipo);
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+	
+	
+	
+}
